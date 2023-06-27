6db00146ea6a49ca36c14e68d7e825de612ea0c3
grx45 <jeffryngasinur@gmail.com>
2023-06-27 16:49:31 +0700

    first card done

--- src/Components/Cards/RectangleCard.jsx
+++ src/Components/Cards/RectangleCard.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import noImage from "../../Assets/noImage.png";
-import { Card, Text, Image, Box, Skeleton } from "@chakra-ui/react";
+import { Card, Image, Box, Text, Heading, Tag, TagLabel, ButtonGroup, Button, Flex, IconButton, textDecoration } from "@chakra-ui/react";
 import { CapitalizeFirstLetter } from "../../Helpers/CapitalizeFirstLetter";
+import { PokemonColor } from "../../Helpers/PokemonColor";
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+
 
 function RectangleCard(props) {
     console.log("fetch data", props.pokemon);
@@ -29,16 +32,38 @@ function RectangleCard(props) {
                     alt="Pokemon"
                     cursor="pointer"
                 />
-
             </Box>
 
 
             <Box width={"70%"} ml={2}>
+                <Heading size='md' mb='2'>{CapitalizeFirstLetter(props.pokemon?.name)}</Heading>
 
-                <Text>{CapitalizeFirstLetter(props.pokemon?.name)}</Text>
+                <Box>
+                    {
+                        props.pokemon?.types?.map((val) => {
+                            return (
+                                <Tag size="md" borderRadius={"full"} key={val} variant={"solid"} bgColor={() => PokemonColor(val?.type?.name)} mr={'1'}>
+                                    <TagLabel fontWeight={"bold"} fontSize={"xs"}>{CapitalizeFirstLetter(val?.type?.name)}</TagLabel>
+                                </Tag>
+                            )
+                        })
+                    }
+                </Box>
+
+                <ButtonGroup justifyContent={"flex-end"} mt='2' w='full'>
+
+                    <Button _hover={{ transform: 'scale(1.2)' }} _active={"none"} color='white' bgColor='inherit'>
+                        <AiOutlineHeart style={{ color: 'white', fontSize: '18px' }} />
+                    </Button>
+                    <Button style={{ transition: "all 0.2s ease 0s" }} variant={"outline"} border={"2px"} borderColor={"Yellow"} _active='none' bgColor='inherit' rounded='lg' textColor={'white'} _hover={{ transition: "all 0.2s ease 0s", borderColor: "#FF7A2E", textDecoration: "underline" }} >
+                        <Text fontSize={'sm'} fontWeight={"bold"}>View Details</Text>
+                    </Button>
+                </ButtonGroup>
+
+
 
             </Box>
-        </Card>
+        </Card >
     );
 }
 
